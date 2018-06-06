107a3c7d49b9c03e17b5d00d7bd67ddf83b4a091
Eduardo <ed.quiroga.2103@gmail.com>
2018-06-05 19:13:15 -0600

    XMLManager añadido

---
+++ Servidor/XMLManager/XMLManager.h
@@ -0,0 +1,14 @@
+//
+// Created by eduardo on 05/06/18.
+//
+
+#ifndef XMLMANAGER_XMLMANAGER_H
+#define XMLMANAGER_XMLMANAGER_H
+
+
+class XMLManager {
+
+};
+
+
+#endif //XMLMANAGER_XMLMANAGER_H
---
+++ Servidor/XMLManager/cmake-build-debug/CMakeFiles/XMLManager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XMLManager.dir/pugixml.cpp.o"
+  "CMakeFiles/XMLManager.dir/main.cpp.o"
+  "CMakeFiles/XMLManager.dir/XMLManager.cpp.o"
+  "XMLManager.pdb"
+  "XMLManager"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XMLManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
